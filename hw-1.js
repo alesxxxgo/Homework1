644f7186239b1e799c2f46124861d2510556f680
alesxxxgo <alesxxxgo@gmail.com>
2026-08-31 15:53:10 +0300

    Completed assigned tasks in JS.

---
+++ hw-1.js
@@ -0,0 +1,80 @@
+//Task 1st
+
+let a = 10;
+
+alert (a);
+
+a = 20;
+
+alert(a);
+
+//Task 2nd
+
+const iphoneReleaseYear = 2007;
+
+alert(iphoneReleaseYear);
+
+//Task 3rd
+
+const jsCreator = "Брэндом Эйх";
+
+alert(jsCreator);
+
+//Task 4th
+
+const num1 = 10;
+const num2 = 2;
+
+alert(num1 + num2);
+alert(num1 - num2);
+alert(num1 * num2);
+alert(num1 / num2);
+
+//Task 5th
+
+const result = 2 ** 5;
+
+alert(result);
+
+//Task 6th
+
+const a1 = 9;
+const b = 2;
+
+const remainder = a1 % b;
+
+alert(remainder);
+
+//Task 7th
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+//Task 8th
+
+const age = prompt("Сколько вам лет?");
+
+alert(age);
+
+//Task 9th
+
+const user = {
+    name: "Иван",
+    age: 25,
+    isAdmin: true
+};
+
+//Task 10th
+
+const userName = prompt("Как вас зовут?");
+
+alert(`Привет, ${userName}!`);
+
+
+
